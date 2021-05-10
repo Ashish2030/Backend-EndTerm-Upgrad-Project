48e8b974b1553b28e8dbcd6e24457cd76c2e3ce3
Ashish2030 <Kumarashish79924@gmail.com>
2021-05-10 06:12:47 +0530

    𝐅𝐢𝐧𝐚𝐥 𝐏𝐫𝐨𝐣𝐞𝐜𝐭

---
+++ src/main/java/com/ashish/TodoList/TodoListApplication.java
@@ -0,0 +1,13 @@
+package com.ashish.TodoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashish/TodoList/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.TodoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
